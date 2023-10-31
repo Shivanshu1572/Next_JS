219abba46cd5f5c6e2f57f949a31c6377fbdaafe
Shivanshu <singhshivanshu@seasiainfotech.com>
2023-10-31 14:10:59 +0530

    next

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const page = () => {
+  return (
+   <section className='flex-start flex-col paddings mb-16'>
+    <h1>Categories</h1>
+    <h1>Posts</h1>
+    <h1>LoadMore</h1>
+    <h1>Categories</h1>
+
+   </section>
+  )
+}
+
+export default page
